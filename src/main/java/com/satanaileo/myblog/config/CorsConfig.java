0083d8a150873a05a4971b44c2d0d0203a92d9c1
satanaileo <1459801310@qq.com>
2022-09-10 08:57:22 +0800

    登录功能的前后端结合实现

--- src/main/java/com/satanaileo/myblog/config/CorsConfig.java
+++ src/main/java/com/satanaileo/myblog/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.satanaileo.myblog.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -7,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * satanaileo
  * 2022/9/7 15:45
  */
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .maxAge(3600)
